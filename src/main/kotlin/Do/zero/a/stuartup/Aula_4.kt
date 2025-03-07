a0a91f351c5156ddd3b9ee4429f7118d57e61566
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2025-03-06 21:54:15 -0300

    Operadores && e ||

---
+++ src/main/kotlin/Do/zero/a/stuartup/Aula_4.kt
@@ -0,0 +1,25 @@
+package Do.zero.a.stuartup
+
+fun main() {
+//    val a: Boolean = true
+//    val b = false
+//    val c = true
+//
+//    // Operador E (&&)
+//    val result1 = a && c
+//    val result2 = b && c
+//
+//    println(result1)
+//    println(result2)
+
+    val a: Boolean = true
+    val b = false
+    val c = true
+
+    // Operador OU (||)
+    val result1 = a || c
+    val result2 = b || c
+
+    println(result1)
+    println(result2)
+}
